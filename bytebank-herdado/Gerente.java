2963e9f4f09711b6522cf01e6ee844e6becdbec0
Marina <marinabrito.ma@gmail.com>
2020-12-27 17:12:24 +0000

    Heranca no Java (extends)

--- bytebank-herdado/Gerente.java
+++ bytebank-herdado/Gerente.java
@@ -1,37 +1,21 @@
-
-public class Gerente {
+// Gerente eh um Funcionario, Gerente herda da class Funcionario
+public class Gerente extends Funcionario{
     
-    private String nome;
-    private String cpf;
-    private double salario;
     private int senha;
 
-    public double getBonificacao() {
-            return this.salario;
+    public void setSenha(int senha) {
+        this.senha = senha;
     }
 
-    public String getNome() {
-        return nome;
+    public boolean autentica(int senha) {
+        if(this.senha == senha){
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
-
+//   public double getBonificacao() {
+//          return this.salario;
+//     }
 }
