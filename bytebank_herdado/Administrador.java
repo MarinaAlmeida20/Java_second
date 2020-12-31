24fbd2c2cb17b3f7a1fa9517e6e8f715c8113f9a
Marina <marinabrito.ma@gmail.com>
2020-12-31 19:42:31 +0000

    criando administrador

---
+++ bytebank_herdado/Administrador.java
@@ -0,0 +1,9 @@
+package Java_second.Java_second.bytebank_herdado;
+
+public class Administrador extends Funcionario {
+
+    @Override
+    public double getBonificacao(){
+        return 50;
+    }
+}
